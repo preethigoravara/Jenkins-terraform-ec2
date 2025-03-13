2932069ff5f48af663d87ccade3a436abc683c9c
arjunachari12 <arjun.achari12@gmail.com>
2025-03-13 10:46:29 +0000

    fixed issue

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "ap-southeast-2"
-}
-
 module "vpc" {
   source = "./modules/vpc"
 }
--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -1,1 +1,3 @@
-ded
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
