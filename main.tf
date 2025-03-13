1d91bd1e5033ac4f5c3f24beb7fae1448e960113
arjunachari12 <arjun.achari12@gmail.com>
2025-03-13 10:36:38 +0000

    added modules

--- main.tf
+++ main.tf
@@ -6,25 +6,36 @@ terraform {
     }
   }
   required_version = ">= 1.2.0"
-
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-arjun12"
-    key            = "terraform.tfstate"
-    region         = "ap-southeast-2"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
 }
 
 provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-09e143e99e8fa74f9"
-  instance_type = "t2.micro"
+module "vpc" {
+  source = "./modules/vpc"
+}
 
-  tags = {
-    Name = "arjun"
-  }
+module "subnet" {
+  source = "./modules/subnet"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "security_group" {
+  source = "./modules/security-group"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "key_pair" {
+  source      = "./modules/key-pair"
+  key_name    = "my-key-arjun2"
+  public_key  = file("./id_rsa.pub")
+}
+
+module "ec2" {
+  source                 = "./modules/ec2"
+  ami                    = var.ami  # <-- Fix: Pass the `ami` variable explicitly
+  subnet_id              = module.subnet.public_subnet_id
+  security_group_id      = module.security_group.security_group_id
+  key_name               = module.key_pair.key_name
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "ap-southeast-2"
+}
+
+variable "ami" {
+  default = "ami-09e143e99e8fa74f9"
+}
+
+
